8f63c2b8ca050574aabda9d7b8b29ff0acb41fb4
Le-Himanshu <veenakum1984@gmail.com>
2021-02-04 17:48:51 +0530

    added

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ var form, player, game;
 var cars, car1, car2, car3, car4;
 
 var track, car1_img, car2_img, car3_img, car4_img;
+// load the images
 
 function preload(){
   track = loadImage("../images/track.jpg");
@@ -22,22 +23,37 @@ function preload(){
 }
 
 function setup(){
+
+
+
+  // creating canvas
+
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
+ // conecting the firebase
+
   database = firebase.database();
+ 
+ // create a game object 
   game = new Game();
+ // read the gamestate
   game.getState();
+  // create the form and take thedetails of player
+
   game.start();
 }
 
 
 function draw(){
+      // if 4 player entre the game change the gamestate to 1 
   if(playerCount === 4){
     game.update(1);
   }
+   // to start the game and show all the cars 
   if(gameState === 1){
     clear();
     game.play();
   }
+ // display in console game eneded
   if(gameState === 2){
     game.end();
   }
